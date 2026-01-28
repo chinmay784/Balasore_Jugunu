1da4e6c157528ec3c55e0b1ab31b1d33939de01f
chinmaypuhan <chinmaypuhan420@gmail.com>
2026-01-28 15:20:42 +0530

    redis_Changed

--- services/auth-service/src/config/redis.js
+++ services/auth-service/src/config/redis.js
@@ -1,12 +1,36 @@
-const Redis = require('ioredis');
+// const Redis = require('ioredis');
 
-const redis = new Redis({
-  host: "localhost",
-  port: 6379,
+// // const redis = new Redis({
+// //   host: "localhost",
+// //   port: 6379,
+// // });
+
+// // const redis = new Redis({
+// //   tls:{
+// //     redis://red-d5stf3s9c44c739i0110:6379
+// //   }
+// // })
+
+// redis.on('connect', () => {
+//   console.log('✅ Redis connected (Auth Service)');
+// });
+
+// redis.on("error", (err) => {
+//   console.error("❌ Redis error", err);
+// });
+
+// module.exports = redis;
+
+
+
+const Redis = require("ioredis");
+
+const redis = new Redis("redis://red-d5stf3s9c44c739i0110.render.com:6379", {
+  tls: {} // REQUIRED for Render
 });
 
-redis.on('connect', () => {
-  console.log('✅ Redis connected (Auth Service)');
+redis.on("connect", () => {
+  console.log("✅ Redis connected");
 });
 
 redis.on("error", (err) => {
